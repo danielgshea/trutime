680125d39566e0f0ae2d3e72b6d0c0a5e55b7b6c
dgshea <dgshea@ncsu.edu>
2024-01-17 13:08:18 -0500

    re working project to be for sorting algorithm practice

---
+++ src/pages/PageHome.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./PageHome.css";
+import { Unstable_Grid2 as Grid } from "@mui/material";
+import { ThemeProvider, styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import useTheme from "@mui/material/styles/useTheme";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+const PageHome: React.FC = () => {
+  const theme = useTheme();
+  const today = new Date().toLocaleDateString();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid xs={6} md={8}>
+            <Item color="primary">
+              <h1>Welcome to calendar-merge</h1>
+            </Item>
+          </Grid>
+          <Grid xs={6} md={4}>
+            <Item>
+              <h2>An app by Daniel</h2>
+            </Item>
+          </Grid>
+          <Grid xs={6} md={4}>
+            <Item>Todays date: {today}</Item>
+          </Grid>
+          <Grid xs={6} md={8}>
+            <Item></Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+export default PageHome;
